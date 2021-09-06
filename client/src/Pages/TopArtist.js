8f8186c7ae654c26dd41710775a2cface5e11db4
levincent51 <levincent51@gmail.com>
2021-09-06 17:24:40 +1000

    work on Reccomendation based on metadata

--- client/src/Pages/TopArtist.js
+++ client/src/Pages/TopArtist.js
@@ -1,27 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { spotifyApi } from "../components/spotifyAPI";
+import { spotifyApi, getAccessToken } from "../components/spotifyAPI";
+
+
+// LOGIC WORKSSS RECURSION
+const getplaylists = async (playlists, limit, offset) => {
+    const newOffset = offset + limit
+    console.log(playlists)
+    const result =  await spotifyApi.getUserPlaylists({
+        limit: limit,
+        offset: offset,
+    }).then((response) => {
+        if (response) {
+            console.log(response)
+            const newPlaylist = [...playlists, ...response.items] ;
+            console.log(newPlaylist)
+            if (response.next) {
+                getplaylists(newPlaylist, limit, newOffset)
+            } 
+        }
+    }).catch(() => {
+        getAccessToken()
+    })
+    return result
+}
+
+
+console.log(getplaylists([],10,0))
+
 
 const Reccommendations = () => {
 
     const [playlists, setPlaylists] = useState();
     const [allSongs, setAllSongs] = useState()
-    spotifyApi.getUserPlaylists( {
-        limit: 1,
-    }).then((response) => {
-        if (response) {
-            console.log(response)
-        }
-        if (response.next) {
-            spotifyApi.getUserPlaylists( {
-                limit: 1,
-                offset: 1
-            }).then((response) => {
-                if (response) {
-                    console.log(response)
-                }
-            })
-        }
-    })
+
+
+    useEffect(() => {
+
+    
+    }, []);
+
+    
+    // neeed recursion of this logic
+
 
     // get every songs form every playlist and every saved 
 
