4ed315091140f1a577dbc326b953c5f3da95dbcd
levincent51 <levincent51@gmail.com>
2021-09-10 10:54:23 +1000

    fix indentation

--- client/src/Pages/Dashboard.js
+++ client/src/Pages/Dashboard.js
@@ -11,68 +11,68 @@ import { spotifyApi, token, getAccessToken } from "../components/spotifyAPI";
 
 
 export default function Dashboard() {
-  // TODO: This takes too many requests, remove for now
-  /*const [isPlaying, setIsPlaying] = useState(false);
-  // Note the spotify web player SDK is in beta so performance is janky
-  
-  useEffect(() => {
-    const interval = setInterval( async () => {
-      spotifyApi.getMyCurrentPlaybackState().then((response) => {
-        if (response) {
-          if (response.is_playing) {
-            setIsPlaying(true);
-          }
-        } else if (!response) {
-          setIsPlaying(false);
-        }
-      })
-      .catch(() => {
-        getAccessToken()
-      });
-    }, 7000);
-    return () => clearInterval(interval);
-  }, [isPlaying]);*/
+// TODO: This takes too many requests, remove for now
+/*const [isPlaying, setIsPlaying] = useState(false);
+// Note the spotify web player SDK is in beta so performance is janky
 
-  //return should return, getTOPTRACKS, make playlist
-  // DashBoard should tell us our favourite genre based on our top artist
-  return (
-    <BrowserRouter>
-      <div /* will make a header bar FOR ALL OUR BROWSER ROUTES*/>
-        <Navbar />
+useEffect(() => {
+	const interval = setInterval( async () => {
+	spotifyApi.getMyCurrentPlaybackState().then((response) => {
+		if (response) {
+		if (response.is_playing) {
+			setIsPlaying(true);
+		}
+		} else if (!response) {
+		setIsPlaying(false);
+		}
+	})
+	.catch(() => {
+		getAccessToken()
+	});
+	}, 7000);
+	return () => clearInterval(interval);
+}, [isPlaying]);*/
 
-        <Switch>
-          <Route path="/RecentTracks">
-            <RecentTracks />
-          </Route>
-          <Route path="/logout">
-            <LogoutButton />
-          </Route>
-          <Route path="/TopTracks">
-            <TopTracks />
-          </Route>
-          <Route path="/Recommendations">
-            <Reccommendations />
-          </Route>
-          <Route path="/">
-            <h1>Welcome to Spoti-FYI</h1>
-          </Route>
-        </Switch>
-        {/*isPlaying ? (
-          <div className="player">
-            <b>Currently Playing</b>
-            <SpotifyPlayer
-              token={token}
-              syncExternalDevice={true}
-              syncExternalDeviceInterval={5}
-            />
-          </div>
-        ) : (
-          <div className="player">
-            <b>Currently Not Playing Anything </b>
-          </div>
-        )*/}
-      </div>
-    </BrowserRouter>
-  );
+//return should return, getTOPTRACKS, make playlist
+// DashBoard should tell us our favourite genre based on our top artist
+	return (
+		<BrowserRouter>
+		<div /* will make a header bar FOR ALL OUR BROWSER ROUTES*/>
+			<Navbar />
+
+			<Switch>
+			<Route path="/RecentTracks">
+				<RecentTracks />
+			</Route>
+			<Route path="/logout">
+				<LogoutButton />
+			</Route>
+			<Route path="/TopTracks">
+				<TopTracks />
+			</Route>
+			<Route path="/Recommendations">
+				<Reccommendations />
+			</Route>
+			<Route path="/">
+				<h1>Welcome to Spoti-FYI</h1>
+			</Route>
+			</Switch>
+			{/*isPlaying ? (
+			<div className="player">
+				<b>Currently Playing</b>
+				<SpotifyPlayer
+				token={token}
+				syncExternalDevice={true}
+				syncExternalDeviceInterval={5}
+				/>
+			</div>
+			) : (
+			<div className="player">
+				<b>Currently Not Playing Anything </b>
+			</div>
+			)*/}
+		</div>
+		</BrowserRouter>
+	);
 }
 // ****** NESTED ROUTING
