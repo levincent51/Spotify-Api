605616ed7e13f903942a166d2d5845577bd3dc05
Vincent Le <levincent51@gmail.com>
2021-11-22 12:39:37 +1100

    test deploy

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
   return (
     <div className="login">
       <p>An insight into your listening habits on Spotify</p>
-      <a href={SERVER_URL}> Login to Spotify </a>
+      <a href={SERVER_URL}> Login to Spotify. </a>
     </div>
   );
 }
