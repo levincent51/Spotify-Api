50652b38470359e1d6a7668b5359ea2dd6a3e489
Vincent Le <levincent51@gmail.com>
2021-11-26 10:21:13 +1100

    a

--- client/src/components/spotifyAPI.js
+++ client/src/components/spotifyAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getHashParams} from "./functions";
 import SpotifyWebApi from "spotify-web-api-js";
 
-const EXPIRATION_TIME = 3600000; // 1 hour
+const EXPIRATION_TIME = 10000; // 1 hour
 const SERVER = "https://react-express-spotify.herokuapp.com/"; // or https://react-express-spotify.herokuapp.com/ http://localhost:8888
 //REFRESH ACCESS TOKEN EVERY 1 HOUR
 // REFRESH_URI
