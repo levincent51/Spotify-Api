f2f9c6fb0f0ac41d9f37927f783379d30d2611af
Vincent Le <levincent51@gmail.com>
2021-11-25 21:50:25 +1100

    a

--- client/src/components/spotifyAPI.js
+++ client/src/components/spotifyAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getHashParams} from "./functions";
 import SpotifyWebApi from "spotify-web-api-js";
 
-const EXPIRATION_TIME = 3600000; // 1 hour
+const EXPIRATION_TIME = 1000; // 1 hour
 const SERVER = "https://react-express-spotify.herokuapp.com/"; // or https://react-express-spotify.herokuapp.com/ http://localhost:8888
 //REFRESH ACCESS TOKEN EVERY 1 HOUR
 // REFRESH_URI
@@ -65,7 +65,9 @@ export const getAccessToken = () => {
     Date.now() - tokenExpiry > EXPIRATION_TIME
   ) {
     console.log("Token expired, refreshing token");
-    return refreshAccessToken();
+    window.location.reload();
+
+    // return refreshAccessToken();
 
   }
 
