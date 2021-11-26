dcfb587b0d5b3f06b1f8a65e481fdad01708a058
Vincent Le <levincent51@gmail.com>
2021-11-26 15:39:59 +1100

    server changes

--- auth-server/authorisation_code/server.js
+++ auth-server/authorisation_code/server.js
@@ -1,4 +1,4 @@
-const CLIENT_SERVER = "https://spoti-fyi.netlify.app/#"; // http://spoti-fyi.netlify.app/# http://localhost:3000/#
+const CLIENT_SERVER = "https://spoti-fyi.netlify.app/#"; //http://spoti-fyi.netlify.app/# http://localhost:3000/#
 var express = require("express"); // Express web server framework
 var request = require("request"); // "Request" library
 var cookieParser = require("cookie-parser");
@@ -32,7 +32,25 @@ var app = express();
 app
   .use(express.static(__dirname + "/public"))
   .use(cookieParser())
-  .use(cors({ origin: true }));
+  .use(cors({ origin: true }))
+  .use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://spoti-fyi.netlify.app/#');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get("/login", (req, res) => {
   // random state
